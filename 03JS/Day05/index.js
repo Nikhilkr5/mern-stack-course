7d932c9133603570706a42471b708b32ec5b8dfa
Nikhil Kumar Thakur <nikhilkumar108thakur@gmail.com>
2026-09-06 17:56:53 +0530

    Refactor number handling examples in index.js
    Uncommented and added explanations for number handling in JavaScript.

--- 03JS/Day05/index.js
+++ 03JS/Day05/index.js
@@ -1,19 +1,43 @@
 // Number
 // let a = 10;
 // let b = 345.6821;
-// let c = b.toFixed(1);
-// console.log(typeof c);
+//console.log(b);
+// let c = b.toFixed(2);  //345.68
+// let c = b.toFixed(1);  //345.7      //it has rounded off
+// console.log(typeof b.toFixed(1));   //string
+// console.log(typeof c);     //string
 
-// console.log(b.toPrecision(4));
-// console.log(b.toString());
+//if i do :
+// b.toFixed(1)    
+//console.log(b);      //b will not get changed        //because of primitive data type(b ka data number hai jo ek primitive data type h)
 
 
-// 
+// console.log(b.toPrecision(5));     //345.68
+// console.log(b.toPrecision(4));      //345.7
+// console.log(b.toString());        //mere pas number hai mai usko Strimg me convert kr skata hu toString ki help se
 
-// let a = new Number(20);
+
+
+
+// hum number ko ek aur tareeke se create kr skte hai:::
+
+// let a = new Number(20);    
 // let b = new Number(20);
 
-// console.log(a==b);
+//console.log(a);      //Output:: [Number : 20]
+//onscole.log(type of a);       //Output:: object
+
+//Jab mai new Keyword use kr rha hu tab ye number to create kr rha h lekin wo usko as an Object le rha h
+//lekin is method ka use hme nhi krna chahie ye bahut hi faltu method hai
+//because : let's see---
+// console.log(a==b);      //Output : false
+//dekho yaha output to true ana chahiye tha lekin aya  false
+//kyuki wo object hai
+//aur object kaise compare hote hai :
+//agar mujhe do object ko compaare krne hai to  wo dono ko dono ek hi value ko point out krne chaiiye
+//un dono ke reference same hone chahiye reference me
+
+//object reference me comparison krta hai
 
 // console.log(Boolean(null));
 
@@ -25,13 +49,15 @@
 //     name:"Rohit"
 // }
 
-// let obj2 = obj1;
-
 // let obj2 = {
 //     name:"Rohit"
 // }
+// console.log(obj1==obj2);   //false    
 
-// console.log(obj1==obj2);
+
+// let obj2 = obj1;   //dono ek hi object ke refernece ko point out kr rhe isliye
+// console.log(obj1==obj2);   //true
+
 
 // Non Primitive: Reference bases pe compare
 // Primitive: Copy by value
